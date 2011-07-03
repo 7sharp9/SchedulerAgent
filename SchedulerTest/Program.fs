80d11a0312ea346f7e940ffef8ca7f9cf0e5c200
Dave Thomas <kukulcanenator@googlemail.com>
2011-07-04 00:35:47 +0100

    Initial version of SchedulerAgent

---
+++ SchedulerTest/Program.fs
@@ -0,0 +1,24 @@
+﻿open AgentUtilities
+open System
+open System.Threading
+
+let scheduler = SchedulerAgent<_>()
+let printer message = 
+    printfn "%s: %s" (DateTime.Now.TimeOfDay.ToString()) message
+
+let singlecancel = scheduler.Schedule(printer, 
+                                      "Hello from the scheduler", 
+                                      TimeSpan(0,0,0,5))
+
+let multicancel = scheduler.Schedule( printer, 
+                                      "Hello from the multi scheduler", 
+                                      TimeSpan(0,0,0,5),
+                                      TimeSpan(0,0,0,0,500))
+
+printfn "Press any key to cancel."
+Console.ReadKey() |> ignore
+
+//Cancel the multi scheduler
+multicancel.Cancel()
+printfn "Cancelled, press any key to exit."
+Console.ReadKey() |> ignore
